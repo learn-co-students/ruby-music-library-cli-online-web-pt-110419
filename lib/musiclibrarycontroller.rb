1d2ae7cbede2f23c23587c8e79ff96a6911a4481
rjr2274 <rjr2274@me.com>
2019-12-18 22:10:53 -0700

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -25,8 +25,77 @@ class MusicLibraryController
    end
 
    def list_songs
-    Songs.all
+    @song_list = Song.all.sort_by {|song|  song.name}
+
+    @song_list.uniq.collect.with_index {|song, index|
+        puts "#{index +1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    }
    end
+
+   def list_artists
+    artist_list = Artist.all.uniq.sort_by{|artist| artist.name}
+
+    artist_list.each_with_index { |artist, index|
+        puts "#{index +1}. #{artist.name}"
+    }
+   end
+
+   def list_genres 
+    genre_list = Genre.all.uniq.sort_by {|genre| genre.name}
+
+
+    genre_list.each_with_index {|genre, index|
+        puts "#{index + 1}. #{genre.name}"
+    }
+   end
+
+   def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    user_input = gets.chomp
+
+    user_artist_songs = Song.all.select {|song| 
+        song.artist.name == user_input    
+    }
+     
+    user_artist_songs = user_artist_songs.uniq.sort_by {|song| song.name}
+
+    user_artist_songs.each_with_index {|song,index|
+        puts "#{index + 1}. #{song.name} - #{song.genre.name}"
+    } 
+   end
+
+   def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    user_input = gets.chomp
+
+    song_list = Song.all.uniq.select {|song| 
+        song.genre.name == user_input
+    }
+
+    song_list = song_list.sort_by {|song|  
+        song.name
+    }
+
+    song_list.each_with_index {|song, index|
+        puts "#{index + 1}. #{song.artist.name} - #{song.name}"
+    }
+
+   end
+
+   def play_song
+    puts "Which song number would you like to play?"
+    user_input = gets.chomp
+    
+    list_songs.select {|song|
+        binding.pry
+         song.index == user_input.to_i
+            
+    }
+    
+
+   end
+
+
 
 
 
