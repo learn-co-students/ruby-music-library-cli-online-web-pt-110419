4a566582b84bc9a9dccf852f44b64592970f0654
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-18 23:20:11 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+class Song 
+  attr_accessor :name
+  attr_reader :artist, :genre 
+  @@all = [] 
+  
+  def initialize(name, artist=nil, genre=nil)
+    @name = name
+   # @artist = artist
+    self.artist= artist if artist 
+    #@genre = genre 
+    self.genre = genre if genre 
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.destroy_all
+    all.clear 
+  end 
+  
+  def self.create(name)
+    song = new(name)
+    song.save 
+    song 
+  end 
+  
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end 
+  
+  def genre=(genre)
+    @genre = genre 
+    genre.songs << self if !genre.songs.include?(self)
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.find_by_name(name)
+    all.detect{|song| song.name == name}
+  end 
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || create(name)
+  end 
+  
+  
+ 
+end 
+  
